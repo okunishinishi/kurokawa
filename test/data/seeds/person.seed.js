5aac19902ed2371bd723dcc9f7613355f8c1bcaf
okunishinishi <okunishinishi@gmail.com>
2013-11-14 06:40:37 +0900

    add data

--- test/data/seeds/person.seed.js
+++ test/data/seeds/person.seed.js
@@ -16,6 +16,8 @@ module.exports = {
             mission:"",
             birthday:"${birthday}",
             birthplace:"${country}",
+            years_of_service:"${randomInt(1,15)}",
+            join_year:'${randomInt(1990,2014)}',
             graduated_from:"${flower}大学"
         }.repeat(100)
     ]
