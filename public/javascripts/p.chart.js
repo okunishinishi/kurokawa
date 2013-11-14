39da6f64682f5b5089663e4757391500bd30410d
okunishinishi <okunishinishi@gmail.com>
2013-11-15 08:59:12 +0900

    add hit word

--- public/javascripts/p.chart.js
+++ public/javascripts/p.chart.js
@@ -7,9 +7,9 @@
  *  Hbs : handlebars
  *
  */
-(function ($, l, Hbs) {
+(function ($, l, hbs) {
     var tmpl = {
-        li: Hbs.templates['chart-list-item']
+        hitWord: hbs.templates['hit-word']
     };
     $.fn.extend({
         sortableTable: function () {
@@ -50,11 +50,94 @@
         },
         sheetTable: function () {
             var table = $(this);
-            table.resize = function (bookWidth) {
-                table.width(bookWidth - 70);
-            };
             table.sortableTable();
             return table;
+        },
+        sheetSearchForm: function (callback) {
+            var form = $(this),
+                searchWordInput = form.findByName('search_word');
+            searchWordInput.textchange(function () {
+                form.submit();
+            });
+            form.submit(function (e) {
+                e.preventDefault();
+                var val = searchWordInput.val();
+                if (val) {
+                    searchWordInput.addClass('search-input-active');
+                } else {
+                    searchWordInput.removeClass('search-input-active');
+                }
+                callback && callback(val);
+            });
+            return form;
+        },
+        filterHit: function (filterword) {
+            var ambiguousMatch = tek.string.ambiguousMatch;
+            var elm = $(this);
+            if (!elm.length) return false;
+            if(elm.is('.hit-word')) return false;
+            var hit = false,
+                contents = elm.contents(),
+                inner = $();
+
+            function hitElement(match) {
+                var origin = match.input,
+                    span = document.createElement('span'),
+                    hit = match[0];
+                span.innerHTML = origin.replace(hit, tmpl.hitWord(hit));
+                span.dataset.origin = origin;
+                return span;
+            }
+
+            for (var i = 0, len = contents.length; i < len; i++) {
+                var content = contents[i];
+                switch (content.nodeType) {
+                    case 3:
+                        var match = ambiguousMatch(filterword, content.nodeValue);
+                        if (match) {
+                            var span = hitElement(match);
+                            content.parentNode.replaceChild(span, content);
+                            hit = true;
+                        }
+                        break;
+                    default:
+                        inner = inner.add($(content));
+                        break;
+
+                }
+            }
+            return inner.filterHit(filterword) || hit;
+        },
+        filterTableRow: function (filterword) {
+            var trs = $(this);
+            for (var i = 0, len = trs.size(); i < len; i++) {
+                var tr = trs.eq(i);
+                var hit = tr.filterHit(filterword);
+                if (hit) {
+                    tr.removeClass('filter-table-row-hidden');
+                } else {
+                    tr.addClass('filter-table-row-hidden')
+                }
+            }
+            return trs;
+        },
+        filterTable: function (fileterword) {
+            var table = $(this),
+                tbody = table.find('tbody');
+            if (fileterword) {
+                tbody.find('tr').filterTableRow(fileterword);
+            } else {
+                tbody.find('.filter-table-row-hidden').removeClass('filter-table-row-hidden');
+            }
+            return table;
+        },
+        leftPage: function () {
+            var page = $(this),
+                inner = $('.book-page-inner', page);
+            page.resize = function (bookWidth) {
+                inner.width(bookWidth - 70);
+            };
+            return page;
         }
     });
 
@@ -63,13 +146,19 @@
             win = $(window);
 
         var book = $('#book', body),
-            sheetTable = $('#sheet-table', body).sheetTable();
+            leftPage = $('#left-page', book).leftPage(),
+            sheetTable = $('#sheet-table', book).sheetTable();
         win.resize(function () {
-            sheetTable.resize(book.width());
+            leftPage.resize(book.width());
         });
-        sheetTable.resize(book.width());
+
+        leftPage.resize(book.width());
         book.trigger('resize-book');
 
         $('#sub-nav', body).subNav('company');
+
+        $('#sheet-search-form', body).sheetSearchForm(function (search_word) {
+            sheetTable.filterTable(search_word);
+        });
     });
 })(jQuery, window['l'], Handlebars);
