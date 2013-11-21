fd565a5ddc4bd330b3d7cc90d591957145dec8a1
okunishinishi <okunishinishi@gmail.com>
2013-11-21 12:24:45 +0900

    modify data

--- test/data/seeds/user.seed.js
+++ test/data/seeds/user.seed.js
@@ -8,14 +8,24 @@ module.exports = {
     /** データ定義 **/
     entries: [
         {
+            _id: "15${padZero(rownum, 22)}",
+            username: "admin",
+            //password is 'pass'
+            password_digest: '17674471281181708515989702442301471436925510125520121625194148173172103992312384320322279197561572162464023771141318920672181681952282281818714710010012696314620132151184',
+            salt: 'ZQfMfvdPhabs2dbox9iT6MFuSQLbWKKHV4EFHM6FpB19zg/mvyxFI837a2sv/ZZAFGC5CSxwuACgtpGnXPn0Zj0pqluTYLkhv2u4zEe9oL6VO1x7SynuQgiakUSOty6xygQ+btE+COxcUXJP1JrQutuy/JsDvDvbP+eICVdPTzs=',
+            real_name: '武田管理',
+            team_id: "90${padZero(rownum%4, 22)}",
+            role: 'admin'
+        },
+        {
             _id: "14${padZero(rownum, 22)}",
-            username: 'user${padZero(rownum,2)}',
+            username: "${toRomaji(name.kana()).toLowerCase().replace(' ','_')}",
             //password is 'pass'
             password_digest: '17674471281181708515989702442301471436925510125520121625194148173172103992312384320322279197561572162464023771141318920672181681952282281818714710010012696314620132151184',
             salt: 'ZQfMfvdPhabs2dbox9iT6MFuSQLbWKKHV4EFHM6FpB19zg/mvyxFI837a2sv/ZZAFGC5CSxwuACgtpGnXPn0Zj0pqluTYLkhv2u4zEe9oL6VO1x7SynuQgiakUSOty6xygQ+btE+COxcUXJP1JrQutuy/JsDvDvbP+eICVdPTzs=',
             real_name: '${name}',
             team_id: "90${padZero(rownum%4, 22)}",
-            role: 'admin'
+            role: "${choice('','admin')}"
         }.repeat(50)
     ]
 };
